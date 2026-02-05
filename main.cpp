5b459c4acfcd8a9623a7c785e5e9236e15d30b7b
Iselin Bittner <iselin.bittner@pm.me>
2026-02-05 14:03:38 +0100

    added user prompt and calculation functionality

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <conio.h>
 
 float calculate_injection_volume(float desired_dose, float vial_concentration) {
+    // error checking
+    if(desired_dose <= 0 || vial_concentration <= 0) return -1;
+    // calculate the resulting injection volume
     float result = desired_dose / vial_concentration;
     return result;
 }
 
 int main(int, char**) {
-    std::cout << "Hello, from EstrogenInjectionCalculator!\n";
-    return 0;
+    float desired_dosage, vial_concentration, result;
+    desired_dosage = -1;
+    vial_concentration = -1;
+    result = -1;
+
+    std::cout << "DIY HRT Injection Volume Calculator\n";
+    std::cout << "-----------------------------------\n";
+    std::cout << "Formula used for calculation: Desired Dose [mg] / Vial Concentration [mg/mL] = Injection Volume [mL]\n\n";
+
+    std::cout << "Please enter your Desired Dose [mg]: \n";
+    std::cin >> desired_dosage;
+    std::cout << "Please enter your Vial Concentration [mg/mL]: \n";
+    std::cin >> vial_concentration;
+
+    result = calculate_injection_volume(desired_dosage, vial_concentration);
+    if(result == -1) {
+        fprintf(stderr, "error calculated value is not usable\n");
+        exit( EXIT_FAILURE );
+    }
+    
+    std::cout << "Your calculated Injection Volume is: " << result << " mL\n";
+    std::cout << "Please double check any results in case you misstyped a value!";
+    std::cout << "Press any key to close... ";
+    _getch();  // Waits for any key press
+    
+    return EXIT_SUCCESS;
 }
